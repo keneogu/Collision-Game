5ea5d62269455d77ee4113c0f42887da9ea5802c
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-08-27 14:34:04 +0100

    Add initialize method that takes the initial variables

--- collision.rb
+++ collision.rb
@@ -4,6 +4,18 @@ set background: 'white'
 set width: 1200
 set height: 800
 
+class Box
+	def initialize
+		@x = rand(Window.width)
+		@y = rand(Window.height)
+		@x_velocity = (-5..5).to_a.sample
+		@y_velocity = (-5..5).to_a.sample
+		@color = Color.new('random')
+	end
+end
+
+$boxes = Array.new(30) {Box.new}
+
 update do
 	clear
 end
